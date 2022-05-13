547f407f0d2fd769a9b19cafc21e558a3d171ae6
Your Name <you@example.com>
2022-05-13 19:59:25 +0700

    123

--- src/customs/sapphire/CatCommand.ts
+++ src/customs/sapphire/CatCommand.ts
@@ -1,18 +1,17 @@
 import type { PieceContext, Args } from '@sapphire/framework';
 import { SubCommandPluginCommand, SubCommandPluginCommandOptions } from '@sapphire/plugin-subcommands';
-
 export abstract class CatCommand extends SubCommandPluginCommand<Args, CatCommand> {
 	public hidden: boolean;
 	public example: string;
-
-	public constructor(context: PieceContext, options: CatCommand.Options) {
+	public constructor(context: PieceContext, options: CatCommand.Options) 
+	{
 		super(context, options);
 		this.hidden = options.hidden ?? false;
 		this.example = options.example ?? ''
 	}
 }
-
-export namespace CatCommand {
+export namespace CatCommand 
+{
 	export type Options = SubCommandPluginCommandOptions & {
 		hidden?: boolean;
 		example?: string;
